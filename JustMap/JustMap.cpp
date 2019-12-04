38618ac26d2c758b0861e30f71c7ede72748145a
Mike Tkachuk <tmymichael@gmail.com>
2019-12-04 18:18:18 +0200

    Castle has been added

--- JustMap/JustMap.cpp
+++ JustMap/JustMap.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-const int H = 20, W = 40;
+const int H = 50, W = 100;
 bool map[H][W];
 
 void setXY(short x, short y)
@@ -37,6 +37,18 @@ bool checkVerticalWall(short x, short y) {
 	return 1;
 }
 
+bool checkCastle(short x, short y) {
+	for (int i = x - 6; i <= x + 7; i++) {
+		for (int j = y - 6; j <= y + 7; j++) {
+			if (map[i][j] == 1) {
+				return 0;
+			}
+		}
+	}
+
+	return 1;
+}
+
 bool checkHorizontalWall(short x, short y) {
 
 	for (int i = x - 1; i <= x + 1; i++) {
@@ -51,14 +63,15 @@ bool checkHorizontalWall(short x, short y) {
 }
 
 //Build walls (0 - vertical, 1 - horizontal)
-void buildWalls() {
-	int chose, x, y;
-	for (int i = 0; i < 30;) {
-		chose = rand() % 2;
+void buildWalls(int count) {
+	int chose, x, y, i;
+	i = 0;
+	for (;i < count;) {
+		chose = rand() % 3;
 		switch (chose) {
-		case 0:
-			x = 2 + rand() % H-2;
-			y = 2 + rand() % W-2;
+		case 24:
+			x = 3 + rand() % H-4;
+			y = 3 + rand() % W-4;
 			if (checkVerticalWall(x, y)) {
 				for (int j = x - 1; j <= x + 1; j++) {
 					map[j][y] = 1;
@@ -66,9 +79,9 @@ void buildWalls() {
 				i++;
 			}
 			break;
-		case 1:
-			x = 2 + rand() % H - 2;
-			y = 2 + rand() % W - 2;
+		case 25:
+			x = 3 + rand() % H - 4;
+			y = 3 + rand() % W - 4;
 			if (checkHorizontalWall(x, y)) {
 				for (int j = y - 1; j <= y + 1; j++) {
 					map[x][j] = 1;
@@ -76,9 +89,37 @@ void buildWalls() {
 				i++;
 			}
 			break;
-		}
-	}
+		case 2:
+			x = 6 + rand() % H - 8;
+			y = 6 + rand() % W - 8;
+			if (checkCastle(x, y)) {
+				for (int l = x - 5; l <= x + 6; l++) {
+					for (int j = y - 5; j <= y + 6; j++) {
+						if (l == x - 5 || j == y - 5 || l == x + 6 || j == y + 6) {
+							map[l][j] = 1;
+						}
+					}
+				}
 
+				for (int l = x - 3; l <= x + 4; l++) {
+					for (int j = y - 3; j <= y + 4; j++) {
+						if (l == x - 3 || j == y - 3 || l == x + 4 || j == y + 4) {
+							map[l][j] = 1;
+						}
+					}
+				}
+
+				map[x + 4][y] = 0;
+				map[x - 5][y] = 0;
+
+
+				i++;
+			}
+			break;
+		}
+		setXY(H + 5, W + 5);
+		cout << "wallsCount:" << setw(3) << i;
+	}
 }
 
 bool check(int x, int y)
@@ -89,6 +130,7 @@ bool check(int x, int y)
 int main()
 {
 	srand(time(NULL));
+
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
 			if (i == 0 || j == 0 || i == H - 1 || j == W - 1) {
@@ -100,7 +142,7 @@ int main()
 		}
 	}
 
-	buildWalls();
+	buildWalls(3);
 
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
@@ -109,10 +151,18 @@ int main()
 				cout << "#";
 			}
 		}
-		cout << endl;
 	}
 
-	int x = H/2, y = W/2;
+	//Set start x, y
+	int x, y;
+	for (;;) {
+		x = 2 + rand() % H-2;
+		y = 2 + rand() % W-2;
+		if (map[x][y] == 0) {
+			break;
+		}
+	}
+
 	bool flag = true;
 	int t = 100;
 	enum Moves { UP = 72, RIGHT = 77, DOWN = 80, LEFT = 75, Enter = 13 };
@@ -163,6 +213,5 @@ int main()
 			break;
 		}
 	}
-	
 }
 
